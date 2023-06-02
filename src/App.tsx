b539c0650b381290572e9ef4da35a98aecb56c6b
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-02 17:21:12 +0200

    Project ready to be integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
-  return (
-    <div>Hello, Thomas</div>
-  )
+  return <div className="">Hello, Thomas</div>;
 }
 
-const container = document.querySelector('#root')
+const container = document.querySelector("#root");
 
 if (!container) {
-  throw new Error("No container to render to")
+  throw new Error("No container to render to");
 }
 
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
-
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
