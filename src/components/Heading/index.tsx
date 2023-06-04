d1bb1e995d1df041092e82d0acdc8fba8fee0a89
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-04 21:05:20 +0200

    Adding font-family and font-weight

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -8,11 +8,11 @@ function Component({
   as: string;
 }) {
   if (as === "h1") {
-    return <h1 className={`${css} text-7xl`}>{content}</h1>;
+    return <h1 className={`${css} font-bold font-space-grotest text-7xl`}>{content}</h1>;
   }
 
   if (as === "h2") {
-    return <h2 className={`${css} text-5xl`}>{content}</h2>;
+    return <h2 className={`${css} font-bold font-space-grotest text-5xl`}>{content}</h2>;
   }
 
   return <h3>Not found</h3>;
