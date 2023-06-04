c9590578c11d57fd2dde93789858d31bf731ceab
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-04 19:11:36 +0200

    Adding describtive name for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const CODGRAY = '#151515'
+const SHAMROCK = '#4EE1A0'
+const MINE_SHAFT = '#242424'
+const ALTO = '#d9d9d9'
+
+
 module.exports = {
   content: ["./src/**/*.{ts,tsx,html}"],
   theme: {
     colors: {
-      primay: '#151515',
-      secondary: '#4EE1A0',
-      teriary: '#242424',
-      customGrey: '#d9d9d9'
+      primay: CODGRAY,
+      secondary: SHAMROCK,
+      teriary: MINE_SHAFT,
+      customGrey: ALTO
     },
     fontFamily: {
       'space-grotest': ['Space Grotesk', 'sans-serif']
