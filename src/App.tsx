ed3af190169ddec2b49c93f1f126f194c29151d3
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-04 19:04:21 +0200

    Defining color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <div className="">Hello, Thomas</div>;
+  return (
+    <div className="bg-primay">
+      <h1 className="text-customGrey">Foo</h1>
+    </div>
+  )
 }
 
 const container = document.querySelector("#root");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx,html}"],
   theme: {
-    extend: {},
+    colors: {
+      primay: '#151515',
+      secondary: '#4EE1A0',
+      teriary: '#242424',
+      customGrey: '#d9d9d9'
+    },
+    fontFamily: {
+      'space-grotest': ['Space Grotesk', 'sans-serif']
+    }
   },
   plugins: [],
 };
