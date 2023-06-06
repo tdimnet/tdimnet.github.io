f32d942502ad9292fa0d2a90bcfcb2a881459ea6
Thomas Dimnet <tdimnet@users.noreply.github.com>
2023-06-06 17:11:48 +0200

    (FEAT) adding header (#4)
    * Designing Header and Main content:
    
    - Inserting Icons.
    - Changing colors from light grey to white
    
    * Playing with layout:
    
    - Adding padding on mobile.
    - Make the Medium icon white.
    - Centering the layout
    
    * Adding animation when hovering icons
    
    * Building another version

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Heading from "../Heading";
+import GithubIcon from "../../icons/Github";
+import MediumIcon from "../../icons/Medium";
+
+function Component() {
+  return (
+    <header className="flex">
+      <Heading css="text-white" content="tom" as="h3" />
+      <a
+        className="ml-auto mr-5 opacity-80 hover:opacity-100 ease-in duration-200 hover:scale-125"
+        href="https://github.com/tdimnet"
+        target="_blank"
+      >
+        <GithubIcon />
+      </a>
+      <a
+        className="opacity-80 hover:opacity-100 ease-in duration-200 hover:scale-125"
+        href="https://tdimnet.medium.com/"
+        target="_blank"
+      >
+        <MediumIcon />
+      </a>
+    </header>
+  );
+}
+
+export default Component;
