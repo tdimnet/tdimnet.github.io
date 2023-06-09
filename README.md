1be406b37d00bc48d0908e52b49c0c3340f354ff
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-09 10:08:36 +0200

    Starting adding the skills section

---
+++ README.md
@@ -0,0 +1,17 @@
+# tdimnet.github.io
+
+My professional portfolio built with React, TypeScript, Vite, and Tailwind.
+
+
+## Prerequisites
+
+- Node (>=18.14)
+
+
+## Setting Up
+
+- Clone the repo
+- Install the dependencies with `yarn`
+- Lauch the dev server with `yarn dev`
+
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 import Heading from "../../components/Heading";
 import Paragraph from "../../components/Paragraph";
 import Header from "../../components/Header";
+import Skill from "../../components/Skill";
 
 function Page() {
   return (
-    <main className="bg-primay h-screen w-screen">
-      <div className="max-w-screen-lg mx-auto pt-5 px-5 lg:px-0 h-full">
-        <Header />
-        <section className="flex justify-center items-center flex-col h-5/6">
+    <main className="bg-primay">
+      <div className="max-w-screen-lg mx-auto pt-5 px-5 lg:px-0 min-h-screen">
+        <div className="mb-36">
+          <Header />
+        </div>
+        <section className="flex justify-center items-center flex-col mb-48">
           <Heading
             css="text-white mb-5"
             content="Hi Friendly Visitor 👋"
@@ -26,6 +29,12 @@ function Page() {
             color="text-white"
             content="Released date: <strong>June 2023</strong>."
           />
+        </section>
+        <section>
+          <Heading css="text-white mb-10 underline underline-offset-4" content="Hard Skills" as="h2" />
+          <ul>
+            <Skill title="HTML" content="7 Years Experience" />
+          </ul>
         </section>
       </div>
     </main>
