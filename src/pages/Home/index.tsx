02939ab46822e5920c131ecfa9513821dc028915
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-07 15:32:29 +0200

    (Feat) project components (#12)
    * Adding project component
    
    * Adding first asset and updating the layout
    
    * Adding other example of projects
    
    * Creating the Project Link component
    
    * Styling the project links
    
    * Finishing the first work on projects
    
    * Running Prettier

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,11 @@ import Heading from "../../components/Heading";
 import Paragraph from "../../components/Paragraph";
 import Header from "../../components/Header";
 import Skill from "../../components/Skill";
+import Project from "../../components/Project";
 
 function Page() {
   return (
-    <main className="bg-primay">
+    <main className="bg-primay pb-16">
       <div className="max-w-screen-lg mx-auto pt-5 px-5 lg:px-0 min-h-screen">
         <div className="mb-36">
           <Header />
@@ -30,13 +31,13 @@ function Page() {
             content="Released date: <strong>June 2023</strong>."
           />
         </section>
-        <section>
+        <section className="mb-12">
           <Heading
             css="text-white mb-10 underline underline-offset-4"
             content="Hard Skills"
             as="h2"
           />
-          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-y-5">
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
             <Skill title="HTML" content="7 Years Experience" />
             <Skill title="CSS" content="7 Years Experience" />
             <Skill title="JavaScript" content="7 Years Experience" />
@@ -44,6 +45,51 @@ function Page() {
             <Skill title="Accessibility" content="5 Years Experience" />
             <Skill title="React/Next" content="5 Years Experience" />
           </ul>
+        </section>
+        <section>
+          <Heading
+            css="text-white mb-10 underline underline-offset-4"
+            content="Projects"
+            as="h2"
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 md:gap-x-5">
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+            <Project
+              title="SensCritique"
+              skills={["NextJS", "React Native", "Python"]}
+              projectUrl="https://www.senscritique.com"
+              codeUrl="https://www.senscritique.com"
+            />
+          </div>
         </section>
       </div>
     </main>
