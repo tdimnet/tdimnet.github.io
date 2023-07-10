9994002b7a933c019af605bfdde21dd22d83cd79
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-10 14:22:09 +0200

    Updating component structure:
    - Wrapping content inside router.
    - Now having the same layout for different pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
 import About from "./pages/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 import Project from "./pages/Project";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/project/:id" element={<Project />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="bg-primay pb-16">
+        <div className="max-w-screen-lg mx-auto pt-5 px-5 lg:px-0 min-h-screen">
+          <div className="mb-5">
+            <Header />
+          </div>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/project/:id" element={<Project />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
+
     </BrowserRouter>
   );
 }
