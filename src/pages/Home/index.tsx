1cb5cb95686fd4b646668cacda5e27ec38fe3a49
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-28 14:01:42 +0200

    (Feat) Updating header links and content (#41)
    * Writing content for the home page
    
    * Updating home page with new wording
    
    * Creating the publications page and updating header links
    
    * Writing new homepage content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,44 +1,59 @@
 import Heading from "../../components/Heading";
 import Image from "../../components/Image";
-import Link from "../../components/Link";
 import Paragraph from "../../components/Paragraph";
-import Project from "../../components/Project";
-import ProjectsGrid from "../../layouts/ProjectsGrid";
-import Skill from "../../components/Skill";
-import SkillsGrid from "../../layouts/SkillsGrid";
-
-import { projects } from "../../data/projects";
-import { calculateYears } from "../../utils/calculateYears";
+import Link from "../../components/Link";
 
 function Page() {
-  const topSixProjects = projects.slice(0, 6);
-
   return (
-    <main className="row-span-5">
+    <main className="row-span-5 lg:mt-36">
       <section className="flex flex-col md:flex-row-reverse">
         <Image
           src="/assets/profil-picture.png"
-          css="mb-5 w-96"
+          css="mb-5 w-96 object-cover"
           alt="Someone wearing a baseball cap in pixel art"
         />
         <div className="mr-10">
-          <Paragraph css="mb-5 text-lg">Thomas "tom" Dimnet</Paragraph>
+          <Paragraph css="mb-3 text-lg">Thomas "tom" Dimnet</Paragraph>
           <Heading
-            css="text-white mb-3"
+            css="text-white mb-2"
             content="Founder - NX Academy"
             as="h1"
           />
           <Heading
-            css="text-white mb-10"
+            css="text-white mb-8"
             content="Software Engineer and Engineering Teacher"
             as="h2"
           />
           <Paragraph css="mb-3">
-            Based in Paris, I am the founder of NX Academy. My goal is to help
-            developpers to meet their objectives, whatever they are. I am
-            interested in many subjects, such as Computer Science, Data Science,
-            Learning and Teaching Science, Psychology and Phylosophy.
+            Based in Paris, I am the founder of{" "}
+            <Link content="NX Academy" to="https://beta.nx.academy/" external />
+            , where my mission is to empower developers to reach their goals,
+            whatever they may be. With a diverse interest in Computer Science,
+            Data Science, Learning and Teaching Sciences, Psychology, and
+            Philosophy, I’m not only passionate about delving into complex
+            topics but also about sharing this knowledge.
           </Paragraph>
+          <Paragraph css="mb-3">
+            Though I occasionally grace the stage at conferences, my true
+            strengths shine through in personal coaching and hands-on coding.
+            Over the years, I’ve honed my skills and expertise across various
+            roles and organizations. My journey includes roles as a Software
+            Engineer and Developer Advocate at Datadog, a Lead Teacher at
+            OpenClassrooms, and a Lead Developer/CTO at SensCritique.
+          </Paragraph>
+          <Paragraph css="mb-5">
+            My commitment to fostering learning and development extends beyond
+            traditional roles - I've been a speaker and instructor at On-Train,
+            IronHack, and several other local bootcamps, where I’ve shared
+            insights and knowledge that bridge the gap between learning and
+            practical application.
+          </Paragraph>
+          {/* <Paragraph css="mb-3">
+            Depending on your objective, you might have a look on{" "}
+            <Link content="Link one" to="#" />,{" "}
+            <Link content="Link two" to="#" />, or{" "}
+            <Link content="Link three" to="#" />.
+          </Paragraph> */}
         </div>
       </section>
     </main>
