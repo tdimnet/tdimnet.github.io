efffd04ce028b017a685306a170cfa1b115ab7ac
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-10-09 17:38:35 +0200

    Adding projects and technologies
     Please enter the commit message for your changes. Lines starting

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -18,4 +18,40 @@ export const technologies: Technology[] = [
     yearStart: 2016,
     img: "/assets/react-logo.png",
   },
+  {
+    id: 3,
+    name: "Node.js",
+    yearStart: 2018,
+    img: "/assets/nodejs-logo.png",
+  },
+  {
+    id: 4,
+    name: "GraphQL",
+    yearStart: 2018,
+    img: "/assets/graphql-logo.png",
+  },
+  {
+    id: 5,
+    name: "Python",
+    yearStart: 2019,
+    img: "/assets/python-logo.png",
+  },
+  {
+    id: 6,
+    name: "Flask",
+    yearStart: 2019,
+    img: "/assets/flask-logo.png",
+  },
+  {
+    id: 7,
+    name: "Docker",
+    yearStart: 2018,
+    img: "/assets/docker-logo.png",
+  },
+  {
+    id: 8,
+    name: "Php",
+    yearStart: 2012,
+    img: "/assets/php-logo.png",
+  },
 ];
