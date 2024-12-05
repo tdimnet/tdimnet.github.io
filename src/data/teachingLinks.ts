79c796e9b44c437d09b17573614025ea60fc7ecb
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-12-05 16:39:50 +0100

    (Feat) teaching page content (#57)
    * Starting to add website content
    
    * Finishing adding content

---
+++ src/data/teachingLinks.ts
@@ -0,0 +1,47 @@
+type link = {
+    content: string
+    href: string
+}
+
+const nxLinks: link[] = [
+    {
+        content: "Conteneurisez vos applications avec Docker",
+        href: "#"
+    },
+    {
+        content: "Créez des pipelines CI/CD avec les GitHub Actions",
+        href: "#"
+    },
+    {
+        content: "Utilisez des paradigmes de programmation",
+        href: "#"
+    }
+]
+
+const ocLinks: link[] = [
+    {
+        content: "Make Your Web Content Accessible",
+        href: "#"
+    },
+    {
+        content: "Utilisez des design patterns en JavaScript",
+        href: "#"
+    },
+    {
+        content: "Preparez-vous aux tests techniques pour devenir developpeur",
+        href: "#"
+    },
+    {
+        content: "Code an Accessible Webiste in HTML and CSS",
+        href: "#"
+    },
+    {
+        content: "Deboguez l'interface de votre site internet",
+        href: "#"
+    }
+]
+
+export {
+    nxLinks,
+    ocLinks
+}
